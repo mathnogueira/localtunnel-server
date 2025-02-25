ad0fa2d553a105889c4652c2f2f797b13de3f597
Matheus Nogueira <matheus.nogueira2008@gmail.com>
2025-02-25 15:55:05 -0300

    allow overriding the agent socket timeout using the server options

--- bin/server.js
+++ bin/server.js
@@ -14,28 +14,32 @@ const argv = optimist
     .usage('Usage: $0 --port [num]')
     .options('secure', {
         default: false,
-        describe: 'use this flag to indicate proxy over https'
+        describe: 'use this flag to indicate proxy over https',
     })
     .options('port', {
         default: '80',
-        describe: 'listen on this port for outside requests'
+        describe: 'listen on this port for outside requests',
     })
     .options('address', {
         default: '0.0.0.0',
-        describe: 'IP address to bind to'
+        describe: 'IP address to bind to',
     })
     .options('domain', {
         describe:
-            'Specify the base domain name. This is optional if hosting localtunnel from a regular example.com domain. This is required if hosting a localtunnel server from a subdomain (i.e. lt.example.com where clients will be client-app.lt.example.com)'
+            'Specify the base domain name. This is optional if hosting localtunnel from a regular example.com domain. This is required if hosting a localtunnel server from a subdomain (i.e. lt.example.com where clients will be client-app.lt.example.com)',
     })
     .options('landing', {
         describe:
-            "Specify the landing page url where users will be redirected to when browsing to the server's domain. This is optional."
+            "Specify the landing page url where users will be redirected to when browsing to the server's domain. This is optional.",
+    })
+    .options('socket-timeout', {
+        default: 60000,
+        describe: 'maximum time to keep a socket alive',
     })
     .options('max-sockets', {
         default: 10,
         describe:
-            'maximum number of tcp sockets each client is allowed to establish at one time (the tunnels)'
+            'maximum number of tcp sockets each client is allowed to establish at one time (the tunnels)',
     }).argv;
 
 if (argv.help) {
@@ -47,7 +51,8 @@ const server = CreateServer({
     max_tcp_sockets: argv['max-sockets'],
     secure: argv.secure,
     domain: argv.domain,
-    landing: argv.landing
+    landing: argv.landing,
+    socketTimeout: argv['socket-timeout'],
 });
 
 server.listen(argv.port, argv.address, () => {
